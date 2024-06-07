7b06c2cd2ceb1b8971b62f1eec4dd99cc2fba839
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-06-07 15:15:10 -0400

    added docker and script

---
+++ run_ontorefine.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Start the services in the background
+docker-compose up -d
+
+# Wait for the server to start
+echo "Waiting for server to start..."
+while ! curl --output /dev/null --silent --head --fail http://localhost:7333; do
+  sleep 5
+done
+echo "Server started!"
+
+# Send a command to the running container
+# https://platform.ontotext.com/ontorefine/ontorefine-cli.html#transform
+echo "Running OntoRefine CLI using config.json..."
+docker exec onto_refine /opt/ontorefine/dist/bin/ontorefine-cli transform ../data/ontorefine/my_sheet.csv \
+  -u http://localhost:7333  \
+  --no-clean \
+  --configurations ../data/ontorefine/sheet-configuration.json  \
+  -f csv >> ontorefine/output.ttl
+
+# Open the default browser
+open http://localhost:7333
+
+echo "Open Project to edit the RDF Mapping."
