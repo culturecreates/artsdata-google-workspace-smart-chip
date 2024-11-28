06a7323ff718300a0650bc91e773b9eec26d09ae
dev <dev.aravind@culturecreates.com>
2024-11-28 16:47:27 +0530

    Removed artsdata search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import artsdataPreview from './artsdata_preview';
 import getEntityDetails from './get_entity_details';
-import onOpen from './onOpen';
-import artsdataSearch from './artsdata_search';
 
 global.artsdataPreview = artsdataPreview;
 global.getEntityDetails = getEntityDetails;
-global.onOpen = onOpen;
-global.artsdataSearch = artsdataSearch;
--- src/onOpen.js
+++
@@ -1,8 +0,0 @@
-const onOpen = () => {
-  SpreadsheetApp.getUi()
-    .createMenu('Artsdata Reconciliation')
-    .addItem('Search', 'artsdataSearch')
-    .addToUi();
-};
-
-export default onOpen;
